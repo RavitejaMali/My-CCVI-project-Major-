3a48a15bb340e2f37b4be3606258dfcd0325a421
RAVITEJA MALI <69043405+RavitejaMali@users.noreply.github.com>
2023-05-02 21:34:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -413,7 +413,18 @@ def preprocessed():
             #return a
             
         return render_template("home.html")
-    
+       
+def download_folder():
+    bucket_contents = s3.list_objects_v2(Bucket=ccvioutputfolder)
+    for item in bucket_contents['Contents']:
+        file_path = item['Key']
+        if not os.path.exists(os.path.dirname(file_path)):
+            os.makedirs(os.path.dirname(file_path))
+        s3.download_file(ccvioutputfolder, file_path, file_path)        
+
+download_folder()
+
+
 if __name__ == "__main__":
     #url="http://localhost:5000/"
     #url = "https://www.google.com"
